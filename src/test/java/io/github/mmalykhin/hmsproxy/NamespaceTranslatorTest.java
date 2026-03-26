b1b87f5281bab712f694d9f1adb5f1b992b34155
Малыхин Максим Владиславович <mmalykhin@beeline.ru>
2026-03-26 18:45:06 +0300

    fix: some fixes

--- src/test/java/io/github/mmalykhin/hmsproxy/NamespaceTranslatorTest.java
+++ src/test/java/io/github/mmalykhin/hmsproxy/NamespaceTranslatorTest.java
@@ -2,9 +2,12 @@ package io.github.mmalykhin.hmsproxy;
 
 import java.util.List;
 import org.apache.hadoop.hive.metastore.api.ColumnStatistics;
+import org.apache.hadoop.hive.metastore.api.ColumnStatisticsData;
 import org.apache.hadoop.hive.metastore.api.ColumnStatisticsDesc;
+import org.apache.hadoop.hive.metastore.api.ColumnStatisticsObj;
 import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.hadoop.hive.metastore.api.GetTableRequest;
+import org.apache.hadoop.hive.metastore.api.LongColumnStatsData;
 import org.apache.hadoop.hive.metastore.api.SetPartitionsStatsRequest;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.junit.Assert;
@@ -105,6 +108,40 @@ public class NamespaceTranslatorTest {
     Assert.assertEquals("sales", routed.getColStats().get(0).getStatsDesc().getDbName());
     Assert.assertNull(routed.getColStats().get(0).getStatsDesc().getCatName());
     Assert.assertEquals("events", routed.getColStats().get(0).getStatsDesc().getTableName());
+  }
+
+  @Test
+  public void internalizeStatsRequestHandlesUnionStatisticsPayload() {
+    ColumnStatisticsDesc statsDesc = new ColumnStatisticsDesc(true, "@hive#catalog1__sales", "events");
+    statsDesc.setCatName("hive");
+
+    LongColumnStatsData longStats = new LongColumnStatsData();
+    longStats.setLowValue(1L);
+    longStats.setHighValue(1L);
+    longStats.setNumDVs(1L);
+    longStats.setNumNulls(0L);
+
+    ColumnStatisticsData statsData = new ColumnStatisticsData();
+    statsData.setLongStats(longStats);
+
+    ColumnStatisticsObj statsObj = new ColumnStatisticsObj();
+    statsObj.setColName("id");
+    statsObj.setColType("int");
+    statsObj.setStatsData(statsData);
+
+    ColumnStatistics statistics = new ColumnStatistics();
+    statistics.setStatsDesc(statsDesc);
+    statistics.setStatsObj(List.of(statsObj));
+
+    SetPartitionsStatsRequest request = new SetPartitionsStatsRequest();
+    request.setColStats(List.of(statistics));
+    request.setNeedMerge(false);
+
+    SetPartitionsStatsRequest routed =
+        (SetPartitionsStatsRequest) NamespaceTranslator.internalizeArgument(request, NAMESPACE);
+
+    Assert.assertEquals("sales", routed.getColStats().get(0).getStatsDesc().getDbName());
+    Assert.assertTrue(routed.getColStats().get(0).getStatsObj().get(0).getStatsData().isSetLongStats());
   }
 
   @Test
